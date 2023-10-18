ebac01d26a10cd23b19d6cb3f3bb2aec2a920844
Chukwudi Wisdom <55758868+override-bot@users.noreply.github.com>
2023-10-18 20:27:04 +0100

    partial cleanup

--- eazyshop/lib/core/viewmodels/seller_viewmodel.dart
+++ eazyshop/lib/core/viewmodels/seller_viewmodel.dart
@@ -1,11 +1,11 @@
-import 'dart:io';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:eazyshop/core/models/seller_model.dart';
+// import 'dart:io';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:eazyshop/core/models/seller_model.dart';
 
 import 'package:flutter/material.dart';
 
-import '../models/user_model.dart';
+// import '../models/user_model.dart';
 
-import '../services/firebase_storage.dart';
+// import '../services/firebase_storage.dart';
 
 class SellerViewmodel extends ChangeNotifier {}
--- eazyshop/lib/core/viewmodels/user_viewmodel.dart
+++ eazyshop/lib/core/viewmodels/user_viewmodel.dart
@@ -1,10 +1,3 @@
-import 'dart:io';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 import 'package:flutter/material.dart';
-
-import '../models/user_model.dart';
-
-import '../services/firebase_storage.dart';
 
 class UserViewmodel extends ChangeNotifier {}
